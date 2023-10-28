fc57770525d942ada9d42569c3d9688589806080
Martin Manchev <mmanchev23@gmail.com>
2023-10-28 17:26:53 +0300

    [Fix] Dynamic state deallocation ...

--- client/src/Shared/Navbar.tsx
+++ client/src/Shared/Navbar.tsx
@@ -1,44 +1,74 @@
-import { Link } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Navbar, Nav, Modal, Button } from "react-bootstrap";
 
 const Navigation = () => {
   const token = sessionStorage.getItem("token");
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setShowConfirmation(true);
+  };
+
+  const confirmLogout = () => {
+    sessionStorage.removeItem("token");
+    setShowConfirmation(false);
+    navigate("/login");
+  };
 
   return (
-    <Navbar bg="light" expand="lg">
-      <div className="container">
-        <Navbar.Brand as={Link} to="/">
-          ZenPayway
-        </Navbar.Brand>
-        <Navbar.Collapse id="navbarNav">
-          <Nav className="mr-auto">
-            {!token && (
-              <>
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to="/register">
-                  Register
-                </Nav.Link>
-              </>
-            )}
-            {token && (
-              <>
-                <Nav.Link as={Link} to="/companies">
-                  Companies
-                </Nav.Link>
-                <Nav.Link as={Link} to="/services">
-                  Services
-                </Nav.Link>
-                <Nav.Link as={Link} to="/logout">
-                  Logout
-                </Nav.Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
+    <>
+      <Modal show={showConfirmation} onHide={() => setShowConfirmation(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Logout</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to log out?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowConfirmation(false)}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={confirmLogout}>
+            Yes, Logout
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Navbar bg="light" expand="lg">
+        <div className="container">
+          <Navbar.Brand as={Link} to="/">
+            ZenPayway
+          </Navbar.Brand>
+          <Navbar.Collapse id="navbarNav">
+            <Nav className="mr-auto">
+              {!token && (
+                <>
+                  <Nav.Link as={Link} to="/login">
+                    Login
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/register">
+                    Register
+                  </Nav.Link>
+                </>
+              )}
+              {token && (
+                <>
+                  <Nav.Link as={Link} to="/companies">
+                    Companies
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/services">
+                    Services
+                  </Nav.Link>
+                  <Nav.Link onClick={handleLogout}>
+                    Logout
+                  </Nav.Link>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    </>
   );
 };
 
