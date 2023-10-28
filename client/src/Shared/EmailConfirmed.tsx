160da1640114139b8fd25661521c1c3f4cafb27c
Aleksandar Valov <28270014+avv73@users.noreply.github.com>
2023-10-28 23:49:30 +0300

    add favicon; soft changes to index

--- client/src/Shared/EmailConfirmed.tsx
+++ client/src/Shared/EmailConfirmed.tsx
@@ -2,12 +2,10 @@ const EmailConfirmed = () => {
     return (
         <div className="container my-5">
             <div className="row">
-                <div className="col-md-6">
+                <div className="col-md-6 mx-auto text-center">
                     <div className="green-background"> {/* Applying green color to the text */}
                         <b>Your email was confirmed! You can now log in.</b>
                     </div>
-                    <h1>Welcome to ZenPayway</h1>
-                    <p>Your One-Stop Destination for Company Services</p>
                 </div>
             </div>
         </div>
